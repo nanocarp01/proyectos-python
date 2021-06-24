861ba84dcae4c0382c8d61e42a71826f647db330
Fernando Heredia <71789047+nanocarp01@users.noreply.github.com>
2021-06-24 14:44:35 -0300

    Update send.py

--- Send Whatsapp/send.py
+++ Send Whatsapp/send.py
@@ -1,3 +1,9 @@
 import pywhatkit as kit
-
-pywhatkit.sendwhatmsg("+549xxxxxxxxxx", "probando whatsapp con python", 19,53)
+print("Antes de ingresar los datos tenga whatsappWeb desde su navegador predefinido sincronizado con su dispositivo")
+print("------- \n" * 5)
+numero=input("ingrese numero de telefono al que quiera mandar Whatsapp ej. 2634845516: ")
+mensaje=input("Ingrese mensaje a enviar: ")
+hora=int(input("ingrese hora en el q desea mandar el mensaje ej.19: "))
+minu=int(input("ingrese minutos en el q desea mandar el mensaje ej.45: "))
+print("El mensaje se enviara automaticamente 20seg despues de que la aplicacion se ha abierto")
+kit.sendwhatmsg("+549"+numero, mensaje, hora,minu)
